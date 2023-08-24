34db6c1443949239c427145b79c7dec0c80414a0
yeo9411 <yeo9411@naver.com>
2023-08-24 17:40:50 +0900

    과제 수정

--- 2023_08_24_02.sql
+++ 2023_08_24_02.sql
@@ -2,24 +2,10 @@
 
 -- 사원테이블(EMP)에서 급여를 3~5번째로 많이 받는 사원을
 -- 출력하는 SQL문을 작성 하세요?
-select rownum, ename, sal from(SELECT  ename, sal 
-from (select ename, sal from emp
-order by sal desc) where rownum <= 5 order by sal asc)
-where rownum <= 3 order by sal desc;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+select rn, ename, sal from(SELECT rownum rn, ename, sal 
+from (select * from emp
+order by sal desc))
+where rn between 3 and 5;
 
 
 
