500c2db9dbadba380efa1c5ab3692fc6496e7c52
yeo9411 <yeo9411@naver.com>
2023-11-06 17:32:53 +0900

    Spring Data Select

---
+++ ClassTest.sql
@@ -0,0 +1,5 @@
+select * from model2member;
+
+select * from model2board order by board_num desc;
+
+select * from mybatisboard;
---
+++ springEX01.sql
@@ -0,0 +1,13 @@
+create table myboard(
+no NUMBER PRIMARY key,
+writer varchar2(20),
+passwd varchar2(20),
+subject varchar2(50),
+content varchar2(100),
+readcount number,
+register date);
+
+create SEQUENCE myboard_seq
+start with 1
+INCREMENT by 1
+NOCACHE;
